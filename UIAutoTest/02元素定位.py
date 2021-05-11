3fc6f9cbbfcf81a18a2ca1a16ae1180e7e5e48f1
liumengyao <734666093@qq.com>
2021-05-12 07:36:19 +0800

    添加selenium基础操作

---
+++ UIAutoTest/02元素定位.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+#  @Time : 2021/5/11 下午9:49
+#  @File : 02元素定位.py
+#  @Software : PyCharm
+#  @Author : Anna
+from selenium import webdriver
+from time import  sleep
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
